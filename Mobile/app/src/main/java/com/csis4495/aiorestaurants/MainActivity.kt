49165325f405d00da5910fd1e33aa61ccf731f77
Eduardo Peres <eduardogeisel@hotmail.com>
2021-06-02 21:54:21 -0700

    Finished first part of design

--- Mobile/app/src/main/java/com/csis4495/aiorestaurants/MainActivity.kt
+++ Mobile/app/src/main/java/com/csis4495/aiorestaurants/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.csis4495.aiorestaurants
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnCashier: Button = findViewById(R.id.btn_cahsier)
+        btnCashier.setOnClickListener {
+            val intent = Intent(this, Cashier::class.java)
+            startActivity(intent);
+        }
+
     }
 }
