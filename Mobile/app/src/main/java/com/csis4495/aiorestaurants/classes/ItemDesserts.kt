2b62d0a001db00d4185ba4d24c19eea3c8585ab3
Eduardo Peres <eduardogeisel@hotmail.com>
2021-06-04 10:02:39 -0700

    Created item desserts class

---
+++ Mobile/app/src/main/java/com/csis4495/aiorestaurants/classes/ItemDesserts.kt
@@ -0,0 +1,13 @@
+package com.csis4495.aiorestaurants.classes
+
+class ItemDesserts {
+
+    var name:String ? = null
+    var price:String ? = null
+
+    constructor(name: String?, price: String?) {
+        this.name = name
+        this.price = price
+    }
+
+}
