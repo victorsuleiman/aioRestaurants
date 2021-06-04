59706bc4b7948a92bfbb782d4baa36589251c4cc
Eduardo Peres <eduardogeisel@hotmail.com>
2021-06-03 21:51:03 -0700

    Created Pizza Item Class

---
+++ Mobile/app/src/main/java/com/csis4495/aiorestaurants/ItemPizza.kt
@@ -0,0 +1,12 @@
+package com.csis4495.aiorestaurants
+
+class ItemPizza {
+
+    var name:String ? = null
+    var price:String ? = null
+
+    constructor(name: String?, price: String?) {
+        this.name = name
+        this.price = price
+    }
+}
