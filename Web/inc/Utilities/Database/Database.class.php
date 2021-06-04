33fb68698a0aa2df2af5f7d49c9604fc834f47ab
Gustavo Freitas <70547481+ac-gbpfreitas@users.noreply.github.com>
2021-06-04 02:29:27 -0700

    Database Abstraction and PDO Files

---
+++ Web/inc/Utilities/Database/Database.class.php
@@ -0,0 +1,47 @@
+<?php
+
+    require_once("PDOMongo.class.php");
+    require_once("PDOAgent.class.php");
+
+    class Database{
+
+        private $connection;
+
+        public function __construct($collection,$selectedDb = "mongo") {
+
+            if(strtolower($selectedDb) == "mysql"){
+
+                try{
+    
+                    if(!empty($collection)){
+                        $this->connection = new PDOAgent($collection);
+                    } else {
+                        throw new Exception("Please, select a class to work with!");
+                    }
+
+                } catch (Exception $errorMessage){
+                    echo $errorMessage->getMessage();
+                }
+
+            } else {
+
+                try{
+
+                    if(!empty($collection)){
+                        $this->connection = new PDOMongo($collection);
+                        //$this->connection->setCollection(strtolower($collection));
+                    } else {
+                        throw new Exception("Please, set a collection to work with!");
+                    }
+                   
+                } catch (Exception $errorMessage){
+                    echo $errorMessage->getMessage();
+                }
+            }
+            
+        }
+
+        public function getDataBase(){
+            return $this->connection;
+        }
+    }
