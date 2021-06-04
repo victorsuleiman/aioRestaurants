30d729a4f0f2e218383946fce6e307f9a283ee57
Eduardo Peres <eduardogeisel@hotmail.com>
2021-06-04 09:57:44 -0700

    Created item sides class

---
+++ Mobile/app/src/main/java/com/csis4495/aiorestaurants/classes/ItemSides.kt
@@ -0,0 +1,13 @@
+package com.csis4495.aiorestaurants.classes
+
+class ItemSides {
+
+    var sidesName:String ? = null
+    var sidesPrice:String ? = null
+
+    constructor(sidesName: String?, sidesPrice: String?) {
+        this.sidesName = sidesName
+        this.sidesPrice = sidesPrice
+    }
+
+}
