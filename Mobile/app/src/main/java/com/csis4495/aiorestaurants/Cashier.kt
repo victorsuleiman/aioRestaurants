03e6b7acdde10e8ce51074a3975ea8687591430a
Eduardo Peres <eduardogeisel@hotmail.com>
2021-06-03 22:12:22 -0700

    Added onclicklistener on home image

--- Mobile/app/src/main/java/com/csis4495/aiorestaurants/Cashier.kt
+++ Mobile/app/src/main/java/com/csis4495/aiorestaurants/Cashier.kt
@@ -1,11 +1,21 @@
 package com.csis4495.aiorestaurants
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class Cashier : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cashier)
+
+        val imgHome: ImageView = findViewById(R.id.imageViewHome)
+        imgHome.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent);
+        }
+
     }
 }
