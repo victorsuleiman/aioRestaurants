36cbc87f627e80d63bcd6796d7bc75ee719ea072
Eduardo Peres <eduardogeisel@hotmail.com>
2021-06-03 21:47:47 -0700

    Created Pizza Grid View

---
+++ Mobile/app/src/main/java/com/csis4495/aiorestaurants/Cashier2.kt
@@ -0,0 +1,11 @@
+package com.csis4495.aiorestaurants
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Cashier2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cashier2)
+    }
+}
