5f0b931c621aa471afc45c05e6f1afe5c000234a
czzbandicoot <victorsuleiman.1994@gmail.com>
2021-06-06 11:37:20 -0700

    Masked Atlas connection with dotenv

--- Mobile Backend/posRoutes.py
+++ Mobile Backend/posRoutes.py
@@ -1,13 +1,17 @@
 from flask import Flask, render_template,  request, escape
 from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb+srv://admin:admin@cluster0.usuw1.mongodb.net/aioRestaurants?retryWrites=true&w=majority" # replace the URI with your own connection
+app.config["MONGO_URI"] = os.getenv('MONGO_URI') # replace the URI with your own connection
 mongo = PyMongo(app)
 
-@app.route('/')
-def init():                            # this is a comment. You can create your own function name
-    return '<h1> {} </h1>'.format(__name__)
+# @app.route('/')
+# def init():                            # this is a comment. You can create your own function name
+#     return '<h1> {} </h1>'.format(__name__)
 
 def getDishes():
     return mongo.db.dish.find({}).limit(1)
@@ -15,6 +19,6 @@ def getDishes():
 dishes = list(getDishes())
 print(dishes[0]['ingredients'][6]['name'])
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0',debug=True)
 
