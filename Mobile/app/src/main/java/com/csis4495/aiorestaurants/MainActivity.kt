cb7b2c8002e2c15a90941d72cbbc60724ae0a1ad
Eduardo Peres <eduardogeisel@hotmail.com>
2021-06-15 18:09:18 -0700

    Added onclick for the logout image

--- Mobile/app/src/main/java/com/csis4495/aiorestaurants/MainActivity.kt
+++ Mobile/app/src/main/java/com/csis4495/aiorestaurants/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,10 +18,18 @@ class MainActivity : AppCompatActivity() {
         //end hiding status bar
         setContentView(R.layout.activity_main)
 
+        //image that logs the user out - goes to logout activity
+        val imgLogout: ImageView = findViewById(R.id.imageViewLogout)
+        imgLogout.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        //button that goes to cashier activity
         val btnCashier: Button = findViewById(R.id.btn_cahsier)
         btnCashier.setOnClickListener {
             val intent = Intent(this, Cashier::class.java)
-            startActivity(intent);
+            startActivity(intent)
         }
 
     }
