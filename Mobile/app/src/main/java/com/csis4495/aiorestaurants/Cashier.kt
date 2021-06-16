615c597d78521f1741c26bdb337a6925c399070a
Eduardo Peres <eduardogeisel@hotmail.com>
2021-06-15 17:49:20 -0700

    Removed status bar from activities

--- Mobile/app/src/main/java/com/csis4495/aiorestaurants/Cashier.kt
+++ Mobile/app/src/main/java/com/csis4495/aiorestaurants/Cashier.kt
@@ -3,12 +3,19 @@ package com.csis4495.aiorestaurants
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.ImageView
 
 class Cashier : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //hiding status bar
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        //end hiding status bar
         setContentView(R.layout.activity_cashier)
 
         //going back to home page when clicking on home image
--- Mobile/app/src/main/java/com/csis4495/aiorestaurants/MainActivity.kt
+++ Mobile/app/src/main/java/com/csis4495/aiorestaurants/MainActivity.kt
@@ -3,11 +3,18 @@ package com.csis4495.aiorestaurants
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //hiding status bar
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        //end hiding status bar
         setContentView(R.layout.activity_main)
 
         val btnCashier: Button = findViewById(R.id.btn_cahsier)
