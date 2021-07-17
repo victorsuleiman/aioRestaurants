ee933c08d8a1417748cd68d9562c869835ca0944
Gustavo Freitas <70547481+ac-gbpfreitas@users.noreply.github.com>
2021-07-16 17:18:43 -0700

    Login page

---
+++ Web/login.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once("inc/Utilities/RestAPI.class.php");
+require_once("inc/Utilities/LoginManager.class.php");
+require_once("inc/Utilities/Page.class.php");
+  
+if(!empty($_POST)){
+
+    if( isset($_POST['usernameL'])){
+        $username = $_POST['usernameL'];
+        $authUser = RestAPI::getUser($username);
+        
+        if (
+            (gettype($authUser) == "object") &&
+            (get_class($authUser) == "UserSession")
+        ) {
+            LoginManager::login($authUser);
+        }
+    }
+} else {
+    LoginManager::logout();
+}
+
+Page::pageHeader();
+Page::leftMenuGuest();
+
+Page::pageContentTop();
+
+Page::loginForm();
+
+Page::pageContentBottom();
+Page::pageFooter();
