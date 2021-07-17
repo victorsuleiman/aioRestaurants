7a361ad1ebe65afa3d196ae971a41f0361a6f49f
Gustavo Freitas <70547481+ac-gbpfreitas@users.noreply.github.com>
2021-07-16 17:24:03 -0700

    New Login Class

---
+++ Web/inc/Utilities/LoginManager.class.php
@@ -0,0 +1,64 @@
+<?php
+
+date_default_timezone_set('America/Vancouver');
+
+    class LoginManager{
+        private static $login;
+
+        public static function checkLogin(){
+            self::$login = false;
+            if(!isset($_SESSION)){
+                session_start();
+            }
+            if(
+                isset($_SESSION['loggedin']) &&
+                self::isLoginSessionExpired()
+            ){
+                self::$login = true;
+            } else {
+                session_destroy();
+                self::$login = false;
+                header("Location: login.php");
+            }
+            return self::$login;
+        }
+
+        public static function login(UserSession $authUser){
+            if($authUser->checkPassword($_POST['password'])){
+                session_start();
+
+                $_SESSION["loggedin"] = true;
+                $_SESSION['username'] = $authUser;
+
+                $time = new DateTime();
+                $time->add(new DateInterval('PT' . 10 . 'M'));
+                $_SESSION['stamp'] = $time->format('H:i');
+
+                header("Location: index.php?page=dashboard");
+                exit();
+            } else {
+                Page::modalMessage("Wrong password or username!");
+            }
+        }
+
+        public static function logout(){
+            session_start();
+            unset($_SESSION["loggedin"]);
+            unset($_SESSION["username"]);
+            $url = "login.php";
+
+            if(isset($_GET["session_expired"])) {
+                $url .= "?session_expired=1";
+            }
+            session_destroy();
+        }
+
+        private static function isLoginSessionExpired() {
+            if( isset($_SESSION["username"]) ){  
+                if(date("H:i") <= $_SESSION['stamp']){ 
+                    return true; 
+                } 
+            }
+            return false;
+        }
+    }
