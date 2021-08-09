66da727bcddf3b4e23312d43243f5d56ed1f7c82
Gustavo Freitas <70547481+ac-gbpfreitas@users.noreply.github.com>
2021-08-09 01:50:48 -0700

    Last Update

--- Web/inc/config.inc.php
+++ Web/inc/config.inc.php
@@ -6,9 +6,3 @@ define("MONGO_DB_USER","admin");
 define("MONGO_DB_PASS","admin");
 define("MONGO_DB_NAME","aioRestaurants");
 define("MONGO_DB_HOST","@cluster0.usuw1.mongodb.net");
-
-
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_NAME','Lab10');
-define('DB_PASS','');
